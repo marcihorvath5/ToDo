3c0d94ca6bfa2d0e25762d0d1df12258139a90e0
Horváth Márton <marcihorat@gmail.com>
2024-08-25 23:09:38 +0200

    #2 Saját adatmodell létrehozása és használata

--- ToDoApp/ToDoApp/Controllers/HomeController.cs
+++ ToDoApp/ToDoApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ToDoApp.Models;
 
 namespace ToDoApp.Controllers
 {
@@ -30,14 +31,13 @@ namespace ToDoApp.Controllers
         public ActionResult ToDoList()
         {
             // bevásárló lista
-            var lista = new List<string>();
+            var lista = new List<TodoItem>();
 
-            lista.Add("Só");
-            lista.Add("Cukor");
-            lista.Add("Spagetti");
-            lista.Add("Marhahús");
-            lista.Add("Paradicsom");
-
+            lista.Add(new TodoItem() {Name = "Só", Done = true});
+            lista.Add(new TodoItem() { Name = "Cukor", Done = true });
+            lista.Add(new TodoItem() { Name = "Spagetti", Done = true });
+            lista.Add(new TodoItem() { Name = "Marhahús", Done = false });
+            lista.Add(new TodoItem() { Name = "Paradicsom", Done = false });
             //a ViewBag-be tett adatokat a nézeten ki tudjuk olvasni
             //Figyelem az erősen típusosságot elveszítjük
             //ViewBag.Lista = list;
