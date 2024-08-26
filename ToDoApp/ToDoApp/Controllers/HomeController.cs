c4cc8211396070c0af33e92b111a86fef18c2d4f
Horváth Márton <marcihorat@gmail.com>
2024-08-26 21:09:27 +0200

    #2  Saját Controller létrehozása, action áthelyezése és átnevezése

--- ToDoApp/ToDoApp/Controllers/HomeController.cs
+++ ToDoApp/ToDoApp/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using ToDoApp.Models;
 
 namespace ToDoApp.Controllers
 {
@@ -26,23 +25,6 @@ namespace ToDoApp.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
-        }
-
-        public ActionResult ToDoList()
-        {
-            // bevásárló lista
-            var lista = new List<TodoItem>();
-
-            lista.Add(new TodoItem() {Name = "Só", Done = true});
-            lista.Add(new TodoItem() { Name = "Cukor", Done = true });
-            lista.Add(new TodoItem() { Name = "Spagetti", Done = true });
-            lista.Add(new TodoItem() { Name = "Marhahús", Done = false });
-            lista.Add(new TodoItem() { Name = "Paradicsom", Done = false });
-            //a ViewBag-be tett adatokat a nézeten ki tudjuk olvasni
-            //Figyelem az erősen típusosságot elveszítjük
-            //ViewBag.Lista = list;
-
-            return View(lista);
         }
     }
 }
---
+++ ToDoApp/ToDoApp/Controllers/TodoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDoApp.Models;
+
+namespace ToDoApp.Controllers
+{
+    public class TodoController: Controller
+    {
+        public ActionResult Index()
+        {
+            // bevásárló lista
+            var lista = new List<TodoItem>();
+
+            lista.Add(new TodoItem() { Name = "Só", Done = true });
+            lista.Add(new TodoItem() { Name = "Cukor", Done = true });
+            lista.Add(new TodoItem() { Name = "Spagetti", Done = true });
+            lista.Add(new TodoItem() { Name = "Marhahús", Done = false });
+            lista.Add(new TodoItem() { Name = "Paradicsom", Done = false });
+            //a ViewBag-be tett adatokat a nézeten ki tudjuk olvasni
+            //Figyelem az erősen típusosságot elveszítjük
+            //ViewBag.Lista = list;
+
+            return View(lista);
+        }
+    }
+}
