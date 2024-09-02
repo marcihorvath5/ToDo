d1a9275623c9e00c4bf8127cc4b729fe5edca4f8
Horváth Márton <marcihorat@gmail.com>
2024-09-03 00:39:48 +0200

    ez még az előzőhöz

--- ToDoApp/ToDoApp/Models/ToDoItem.cs
+++ ToDoApp/ToDoApp/Models/ToDoItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,9 @@ namespace ToDoApp.Models
 {
     public class TodoItem
     {
+        [Key]
+        public int Id { get; internal set; }
         public string Name { get; set; }
         public bool Done { get; internal set; }
-        public int Id {get; internal set; }
     }
 }
