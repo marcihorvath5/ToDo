b7017b1d33113c89563d09d9ca121458e7b2cce2
Horváth Márton <marcihorat@gmail.com>
2024-09-03 00:38:58 +0200

    #4 #5 #3
    Saját adatbázis létrehozása. HTML átírása. Controller átírása

--- ToDoApp/ToDoApp/Controllers/TodoController.cs
+++ ToDoApp/ToDoApp/Controllers/TodoController.cs
@@ -9,6 +9,7 @@ namespace ToDoApp.Controllers
 {
     public class TodoController: Controller
     {
+        Db db = new Db();
        
         public ActionResult Index()
         {                       
@@ -16,7 +17,7 @@ namespace ToDoApp.Controllers
             //Figyelem az erősen típusosságot elveszítjük
             //ViewBag.Lista = list;
 
-            return View(MyDb.Lista);
+            return View(db.TodoItems.ToList());
         }
         
         [HttpGet] // ezzel csak a get kéréssel fogja a routing ide írányítani
@@ -32,9 +33,11 @@ namespace ToDoApp.Controllers
             {
                 // Count nem jó most mert ha törlök egyet akkor 2 ugyanolyan elemem lenne
                 // var maxId = MyDb.Lista.Count;
-                var maxId = MyDb.Lista.Max(x => x.Id);
+                //var maxId = MyDb.Lista.Max(x => x.Id);
 
-                MyDb.Lista.Add(new TodoItem() { Id = maxId + 1, Name = name, Done = isDone });
+                db.TodoItems.Add(new TodoItem() {Name = name, Done = isDone });
+                // Adatbázisba írás
+                db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
@@ -54,9 +57,8 @@ namespace ToDoApp.Controllers
             // MyDb.Lista.Where(x => x.Id == id); // ezzel a lambda fügvénnyel szűröm az elemet aminek egyezik az id-vel
             
             //a single csak akkor működik ha pontosan 1 ilyen elem van
-            var item = MyDb.Lista.Single(x => x.Id == id);
-
-            // Ha nem tudom garantálni akkor
+            var item = db.TodoItems.Single(x => x.Id == id);
+            //Ha nem tudom garantálni akkor
             // ha van ilyen elem azzal tér vissza
             // ha nincs ilyen elem akkor 0-al tér vissza
             // var item = MyDb.Lista.SingleOrDefault(x => x.Id == id);
@@ -69,25 +71,35 @@ namespace ToDoApp.Controllers
         [HttpPost] 
         public ActionResult Edit(int id, string name, bool done)
         {
-            var item = MyDb.Lista.Single(x => x.Id == id);
+            var item = db.TodoItems.Single(x => x.Id == id);
             item.Name = name;
             item.Done = done;
+
+            db.SaveChanges();
+
             return RedirectToAction("index");
         }
 
         [HttpGet]
         public ActionResult Delete(int id) 
         {
-            var item = MyDb.Lista.Single(x => x.Id == id);
+            var item = db.TodoItems.Single(x => x.Id == id);
             return View(item);
         }
         [HttpPost]
         public ActionResult DeleteConfirmed(int id)
         {
-            var item = MyDb.Lista.Single(x => x.Id == id);
-            MyDb.Lista.Remove(item);
+            var item = db.TodoItems.Single(x => x.Id == id);
+
+            db.TodoItems.Remove(item);            
+            db.SaveChanges();
+
             return RedirectToAction("Index");
         }
-
+        public ActionResult Details(int id)
+        {
+            var item = db.TodoItems.Single(x => x.Id == id);
+            return View(item);
+        }
     }
 }
---
+++ ToDoApp/ToDoApp/Models/Db.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ToDoApp.Models
+{
+    public class Db: DbContext
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+    }
+}
